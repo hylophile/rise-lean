5a3cb4747580a81e96d1ec2bb02baf35f90118bd
hylo <hylo@posteo.de>
2025-10-10 13:01:02 +0200

    chore: more experimenting (generating side conditions kinda works?)

--- Rise/Experimenting.lean
+++ Rise/Experimenting.lean
@@ -11,6 +11,17 @@ import Rise.Solve
   (check-synth)
 "
 
+-- #eval runSygus  "
+--   (set-logic NIA)
+--   (synth-fun p ((q Int)) Int)
+--   (declare-var q Int)
+--   (assume (> q 0))
+--   (constraint (> (p q) 0))
+--   (assume (= ((p q)) (div q 4))
+--   (check-synth)
+-- "
+
+
 #eval runSygus  "
   (set-logic NIA)
   (declare-fun p ((q Int)) Int)
@@ -51,3 +62,99 @@ import Rise.Solve
   
 "
 
+
+
+#eval runSygus "
+(set-logic ALL)
+
+(declare-var b Int)
+(synth-fun m ((b Int)) Int)
+
+(assume (> b 0))
+(constraint (> (m b) 0))
+(assume (exists ((mp Int)) (= (* mp 4) b)))
+
+(constraint (= (* (m b) 4) b))
+
+(check-synth)
+
+"
+
+#eval runSygus "
+
+(set-logic NIA)
+
+(synth-fun sideCondition ((n Int)) Bool
+  ((B Bool) (I Int))
+  ((B Bool (true false
+              (= I I)
+              (< I I)
+              (and B B)
+              (or B B)
+              (not B)))
+   (I Int (n 0 1 2 3 4 (+ I I) (- I I) (* I I) (mod I I)))))
+
+(constraint (sideCondition 4))
+(constraint (sideCondition 8))
+(constraint (exists ((n Int)) (and (> n 0) (sideCondition n))))
+
+; ∀n . (n>0 )
+(constraint (forall ((n Int))
+  (=> (and (> n 0) (sideCondition n))
+      (exists ((m Int))
+        (and (> m 0)
+             (= m (div n 4))
+             (= n (* 4 m)))))))
+
+(check-synth)
+  
+"
+#eval runSygus "
+
+(set-logic NIA)
+
+(synth-fun sideCondition ((n Int)) Bool
+  ((B Bool) (I Int))
+  ((B Bool ((= I 0) (> n I)))
+   (I Int (n 4 (mod n I)))))
+
+; ∃n.(n>0 ∧ sideCondition(n))
+; i.e., n>0 must exist
+(constraint (exists ((n Int)) (and (> n 0) (sideCondition n))))
+
+; ∀n.((n>0 ∧ sideCondition(n)) ⇒ ∃m.(m>0 ∧ m = n/4 ∧ n = 4*m))
+(constraint (forall ((n Int))
+  (=> (and (> n 0) (sideCondition n))
+      (exists ((m Int))
+        (and (> m 0)
+             (= m (div n 4))
+             (= n (* 4 m)))))))
+
+(check-synth)
+  
+"
+
+#eval runSygus "
+
+(set-logic NIA)
+
+(synth-fun sideCondition ((n Int)) Bool
+  ((B Bool) (I Int))
+  ((B Bool ((= I 0) (> n I)))
+   (I Int (n 0 4 (mod I I)))))
+
+;(constraint (sideCondition 4))
+;(constraint (sideCondition 8))
+(constraint (exists ((n Int)) (and (> n 0) (sideCondition n))))
+
+; ∀n.((n>0 ∧ sideCondition n) ⇒ ∃m.(m>0 ∧ m = n/4 ∧ n = 4*m))
+(constraint (forall ((n Int))
+  (=> (and (> n 0) (sideCondition n))
+      (exists ((m Int))
+        (and (> m 0)
+             (= m (- n 4))
+             (= n (+ m 4)))))))
+
+(check-synth)
+  
+"
