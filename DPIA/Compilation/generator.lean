a50f76abc91593c66858cbc70f4bd505cdffb8fa
amelie <schall@campus.tu-berlin.de>
2026-03-26 11:41:25 +0100

    to apply TranslationToImperative to a non modyfied Phrase -> splits input arguments to a programm and returntype

---
+++ DPIA/Compilation/generator.lean
@@ -0,0 +1,46 @@
+import DPIA.Compilation.TranslationToImperative
+
+private abbrev mkName := Lean.Name.mkSimple
+
+def createOutputParam (outT : PhraseType) : DPIAPhrase :=
+    match outT with
+        | .expr dt _ => match dt with
+                            | .scalar _ | .index _ | .natType | .vector _ _ => mkBvar 0 (mkName "output") (.acc (.array (.nat 1) dt))
+                            | .array _ _ => mkBvar 0 (mkName "output") (.acc dt)
+                            | .pair _ _ => panic! s!"pair types are currently not supported"
+                            | _ => panic! s!"no other types supported. This should not have happened"
+        | _ => panic! s!"the output parameter is supposed to be an expression type"
+
+partial def splitBodyAndParams (p : DPIAPhrase) (ps : List DPIAPhrase) (depth : Nat) : (DPIAPhrase × (List DPIAPhrase)) :=
+    match p with
+        | ⟨.app fn arg, _⟩ => let sub := applySubstitution fn arg
+                         splitBodyAndParams sub ps depth
+        | ⟨.depapp fn arg, _⟩ => let sub := applyDepSubstitution fn arg
+                            splitBodyAndParams sub ps depth
+        | ⟨.lam name (.expr dt a) body, _⟩ => let param := mkBvar depth name (.expr dt a)
+                                        splitBodyAndParams body (param:: ps) (depth+1)
+        | ⟨.deplam name _ body,_⟩ => let param := mkBvar depth name (.expr (.scalar .int) .read)
+                                     splitBodyAndParams body (param :: ps) (depth + 1) -- check again the name
+        | ⟨_, .expr _ _⟩ => (p, ps.reverse)
+        | ⟨_, _⟩ => panic! s!"this should not happen"
+
+def matchOutputType (outType pType : RData) (outParam : DPIAPhrase) : DPIAPhrase :=
+    if outType == pType then outParam
+    else match outType, pType with
+            | .array (.nat 1) lhsT, rhsT => if lhsT == rhsT
+                                                then atAcc  outParam
+                                                            (mkFunctional (.expr (.index (.nat 1)) .read)
+                                                                          (.natAsIndex (.nat 1) {node := .lit (.int 1), type := .expr .natType .read}))
+                                                else panic! s!"{lhsT} and {rhsT} should match"
+            | _, _ => panic! s!"{outType} and {pType} should match"
+
+partial def toImperative (outParam p : DPIAPhrase) : DPIAPhrase :=
+    let output := matchOutputType (getDataType outParam.type) (getDataType p.type) outParam
+    acc p output 0
+
+
+partial def applyToImp (p : DPIAPhrase ) : DPIAPhrase := --for testing the acc function
+    let (body, _) := splitBodyAndParams p [] 0
+    let outParam := createOutputParam body.type
+    dbg_trace s!"this is the body: {body}"
+    toImperative outParam body
